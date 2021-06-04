7780500438e4f419e95272ba438035db2634f983
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-06-04 15:29:59 -0500

    Add css to style the page

--- js/scripts.js
+++ js/scripts.js
@@ -15,20 +15,24 @@ $("document").ready(function(){
     if (experience !== '3' && projects !== '4' && size ==='1'){
       language = "Python"
       $(".logo1").show();
+      $("#sentence").show();
     }
     else if(experience !== '3' && projects !== '4' && size ==='2'){
       language = "JavaScript";
       $(".logo2").show();
+      $("#sentence").show();
     }
     
     else if(experience !== '1' && projects !== '3' && size !=='1'){
       language = "C#";
       $(".logo3").show();
+      $("#sentence").show();
     
     }
     else {
       language ="Java"
       $(".logo4").show();
+      $("#sentence").show();
     }
     $("#language").text(language) 
     
