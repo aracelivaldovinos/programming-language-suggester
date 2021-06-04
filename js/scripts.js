77798d1c7a375e624caa0b9d6648e2fe7d51b425
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-06-04 11:44:07 -0500

    Add branching to provide return value to the user

--- js/scripts.js
+++ js/scripts.js
@@ -6,6 +6,17 @@ $("document").ready(function(){
     const team = $("select#team").val();
     const size = $("select#size").val();
     const reason = $("select#reason").val();
+    let language = ""
+
+    if (experience === '1'||'2' && projects === '1'||'2' && team==='1'||'4'||'5' && size ==='1'||'2' && reason==='1'||'2'){
+      language = "Python"
+    }
+    else if(experience === '1'||'2'||'3' && projects === '1'||'2'||'3' && team==='2'||'3'||'4' && size ==='2'||'3' && reason==='1'||'2'||'3'){
+      language = "JavaScript"
+    }
+    else if(experience === '3'||'4' && projects === '2'||'4' && team==='3'||'4' && size ==='3' && reason==='3'||'4'){
+      language = "C#"
+    }
 
   });
 
