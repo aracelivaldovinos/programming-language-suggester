eec9cfc1c22fad494409add944804d5d4134876c
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-06-04 12:39:15 -0500

    BUGFIX: return value is returning only one value. The && and || in the if-else statements are corrected. '

--- js/scripts.js
+++ js/scripts.js
@@ -8,17 +8,18 @@ $("document").ready(function(){
     const reason = $("select#reason").val();
     let language = ""
 
-    if (experience === '1'||'2' && projects === '1'||'2' && team==='1'||'4'||'5' && size ==='1'||'2' && reason==='1'||'2'){
+    if (experience !== '3' && projects !== '4' && size ==='1'){
       language = "Python"
     }
-    else if(experience === '1'||'2'||'3' && projects === '1'||'2'||'3' && team==='2'||'3'||'4' && size ==='2'||'3' && reason==='1'||'2'||'3'){
+    else if(experience !== '3' && projects !== '4' && size ==='2'){
       language = "JavaScript"
     }
-    else if(experience === '3'||'4' && projects === '2'||'4' && team==='3'||'4' && size ==='3' && reason==='3'||'4'){
+    
+    else if(experience !== '1' && projects !== '3' && size !=='1'){
       language = "C#"
     }
     else {
-      language ="learn something else"
+      language ="JAVA"
     }
     $("#language").text(language)
 
