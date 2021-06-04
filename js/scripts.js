61337e8b186740d1b03b4bbae49dbd7347202bf0
Araceli Valdovinos <valdovinosaraceli50@gmail.com>
2021-06-04 11:11:41 -0500

    Add variables to store users input

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 $("document").ready(function(){
   $("form#formOne").submit(function(event){
     event.preventDefault();
+    const experience = $("select#experience").val();
+    const projects = $("select#projects").val();
+    const team = $("select#team").val();
+    const size = $("select#size").val();
+    const reason = $("select#reason").val();
 
   });
 
